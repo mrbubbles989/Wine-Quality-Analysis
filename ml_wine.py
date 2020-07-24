7565df3a13521f62bdef023ea1ae6392673db019
mrbubbles989 <43306775+mrbubbles989@users.noreply.github.com>
2020-07-23 23:59:30 -0400

    Update ml_wine.py

--- ml_wine.py
+++ ml_wine.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import plotly.express as px
 
 # Load red wine data
-dataset_url = pd.read_csv('https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009?select=winequality-red.csv')
+df = pd.read_csv("C:/Python/Learning/winequality-red.csv")
 
 #Visualizing the data
 # See the number of rows and columns
@@ -14,3 +14,34 @@ print("Rows, columns: " + str(df.shape))
 
 # See the first 5 rows
 df.head()
+
+# Missing Values 
+print(df.isna().sum()))
+
+# Making a histogram of 'quality' variable
+fig= = px.histogram(df,x='quality')
+fig.show()
+
+# Making a diagram showing weights that would affect the quality of the wine
+corr = df.corr()
+matplotlib.pyplot.subplots(figsize=15,10))
+sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns,
+            annot=True, cmap=sns.diverging_palette(220, 20, as_cmap=True))
+
+# Create Classification version of target variable
+df['goodquality'] = [1 if x >= 7 else 0 for x in df['quality']]
+
+# Seperate feature variables and targe variable
+X = df.drop(['quality', 'goodquality'], axis = 1)
+y = df['goodquality']
+
+# See proportion of good vs bad wines 
+df['goodquality'].value_counts()
+
+# Normalize feature variables 
+from sklearm.preprocessing import StandardScaler
+X_features = X
+X = StandardScaler().fit_transform(X)
+
+# Splitting the data
+
